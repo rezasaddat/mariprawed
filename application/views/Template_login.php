a67a85d78f779155df7d9cc6d004f1a429ab2c9e
Muhamad Reza Saddat <31197736+rezasaddat@users.noreply.github.com>
2018-07-30 21:43:17 +0700

    up

---
+++ application/views/Template_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PRAWEDDING</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/uikit-rtl.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/uikit.css">
+  </head>
+
+<body style="background-image: url('assets/image/bride-ceremony-chairs-794254.jpg'); background-size: cover; background-position: center;">
+        <div class="uk-container">
+            <?php echo $content?>
+        </div>
+</body>
+</html>
+
+<script rel="javascript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script rel="javascript" type="text/javascript" src="<?php echo base_url();?>assets/js/uikit.js"></script>
+<script rel="javascript" type="text/javascript" src="<?php echo base_url();?>assets/js/uikit-icons.js"></script>
