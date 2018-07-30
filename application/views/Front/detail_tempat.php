2e71abee2d9d68477a3989ede8cc01ac172ad8c2
Muhamad Reza Saddat <31197736+rezasaddat@users.noreply.github.com>
2018-07-30 21:14:32 +0700

    up

--- application/views/Front/detail_tempat.php
+++ application/views/Front/detail_tempat.php
@@ -16,6 +16,8 @@
 
                     
                 </ul>
+                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
             </div>
         </div>
     </div>
